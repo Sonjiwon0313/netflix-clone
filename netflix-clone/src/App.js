ad8c8a35b74a37b576035f0e0e22f3bb4d138eed
Sonjiwon0313 <70885246+Sonjiwon0313@users.noreply.github.com>
2023-06-08 11:16:10 +0900

    밤샘

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,14 +1,19 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
 import Row from "./Row";
 import requests from "./Requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Nav />
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
